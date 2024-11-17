18eb9e2a550dfe19cac7fd0b5fe515e078dbc28b
Apple <apple@Apples-MacBook-Air-2.local>
2024-11-17 13:40:00 +0530

    dashboard ui fixes

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -3,10 +3,12 @@ import { ShoppingBag } from 'lucide-react';
 import React from 'react'
 import Rating from '../(components)/Rating';
 import Image from 'next/image';
+import { useAppSelector } from '../redux';
 
 
 const CardPopularProducts = () => {
     const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
   return (
     <div className='row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16'>
         {isLoading ? (
@@ -19,7 +21,7 @@ const CardPopularProducts = () => {
                 {dashboardMetrics?.popularProducts.map((product) => (
                     <div key={product.productId} className='flex items-center justify-between gap-3 px-5 py-7 border-b'>
                         <div className='flex items-center gap-3'>
-                        <Image src={`https://s3-inventorymanagement-priksh.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random() * 3) + 1}.png`} alt={product.name} height={150} width={150} className='rounded-2xl mb-3 h-36 w-36'/>
+                        <Image src={`https://s3-inventorymanagement-priksh.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random() * 3) + 1}.png`} alt={product.name} height={50} width={50} className='rounded-2xl mb-3 h-36 w-36'/>
                             <div className='flex flex-col justify-between gap-1'>
                                 <div className='font-bold text-gray-700'>{product.name}</div>
                                 <div className='flex text-sm items-center'>
@@ -31,7 +33,8 @@ const CardPopularProducts = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='text-sm flex items-center'>
+                        
+                        <div className={`text-sm flex items-center ${isSidebarCollapsed ? 'block': 'hidden'}`}>
                             <button className='p-2 rounded-full bg-blue-100 text-blue-500 mr-2'>
                                 <ShoppingBag className='w-4 h-4'/>
                             </button>
